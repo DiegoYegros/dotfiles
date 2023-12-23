47dc6896588e75a31cb5b70510731cb0a789c7e6
alezkar <diego>
2023-12-23 01:56:02 -0300

    powerline-shell

--- script.sh
+++ script.sh
@@ -7,10 +7,13 @@ sudo apt update
 sudo apt install alacritty -y
 
 # Install picom
-sudo apt install picom 
+sudo apt install picom -y 
 
 # Install scrot for taking screenshots
 sudo apt install scrot -y
+
+# Just making sure you have python3
+sudo apt install python3 -y
 
 # Install psutil for the bumblebee-status CPU module
 sudo apt install python3-psutil -y
@@ -20,11 +23,18 @@ sudo apt install fonts-font-awesome -y
 
 # Install feh
 sudo apt-get install -y feh 
+
 # Install pygit2 for the git module in bumblebee-status
 # Since pygit2 is a Python library, we use pip to install it
 # Ensure pip is installed
 sudo apt install python3-pip -y
+
 pip3 install pygit2
+
+# Install powerline-shell for ofc a pretty shell
+git clone https://github.com/b-ryan/powerline-shell
+cd powerline-shell
+python setup.py install
 
 echo "All required packages have been installed."
 
