b00d105e84e7d50c07ea5d88054c32e368312f67
alezkar <diego>
2023-12-26 07:06:58 -0300

    con

--- vpn-connect.sh
+++ vpn-connect.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-sudo openvpn --config /home/alezkar/Downloads/vpnfile.ovpn
+VPN_CONFIG="/home/alezkar/Downloads/vpnfile.ovpn"
+VPN_CONTROL_FILE="/tmp/vpn_connected"
+
+if [ ! -f "$VPN_CONTROL_FILE" ]; then
+	echo "Starting VPN..."
+	openvpn --config "$VPN_CONFIG" &
+	echo $! >"$VPN_CONTROL_FILE"
+else
+	echo "Stopping VPN..."
+	if [ -s "$VPN_CONTROL_FILE" ]; then
+		VPN_PID=$(cat "$VPN_CONTROL_FILE")
+		kill "$VPN_PID"
+	fi
+	rm "$VPN_CONTROL_FILE"
+fi
