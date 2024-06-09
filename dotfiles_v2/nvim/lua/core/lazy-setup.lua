ebe2f9c2c6518818343d8faec1fa2c46c8ba445d
diegoyegros <diegoyegrosa@gmail.com>
2024-06-09 01:24:55 -0400

    refactor git config

---
+++ dotfiles_v2/nvim/lua/core/lazy-setup.lua
@@ -0,0 +1,28 @@
+-- Manage dependencies using Lazy.
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>L', '<cmd>Lazy<CR>', { noremap = true, silent = true })
+
+require('lazy').setup('plugins', {
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
+  install = {
+    colorscheme = { 'visual_studio_code', 'default' },
+  },
+  ui = {
+    border = 'rounded',
+  },
+})
---
+++ dotfiles_v2/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  opts = {
+    ensure_installed = require('core.languages').parsers,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } },
+  },
+  config = function(_, opts)
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup(opts)
+  end,
+}
