e2f10e4e554370852b30ac4fb81113342c57a918
diegoyegros <diegoyegrosa@gmail.com>
2024-07-20 12:26:48 -0400

    dual monitor setup

--- dotfiles_v2/scripts/monitor-setup.sh
+++ dotfiles_v2/scripts/monitor-setup.sh
@@ -3,16 +3,21 @@ monitor_setup() {
 	monitor_count=$(xrandr --query | grep " connected" | grep -v "disconnected" | wc -l)
 
 	single_monitor_script="$HOME/.config/scripts/screenlayout_single_monitor.sh"
-	multi_monitor_script="$HOME/.config/scripts/screenlayout_triple_monitor.sh"
+	dual_monitor_script="$HOME/.config/scripts/screenlayout_dual_monitor.sh"
+	triple_monitor_script="$HOME/.config/scripts/screenlayout_triple_monitor.sh"
 
 	case $monitor_count in
 	1)
 		echo "Single monitor detected"
 		$single_monitor_script
 		;;
+	2)
+		echo "Two monitors detected"
+		$dual_monitor_script
+		;;
 	3)
 		echo "Three monitors detected"
-		$multi_monitor_script
+		$triple_monitor_script
 		;;
 	*)
 		echo "Unexpected number of monitors: $monitor_count"
