105edf777514b525b78cb22bdd9435ff0d0bdbce
Alezkar <diegoyegrosa@gmail.com>
2024-09-16 10:43:20 -0400

    added symbolic links in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 snap_deps=(
 	"go"
 	"alacritty"
-	)
+)
 
 apt_deps=(
 	"npm"
@@ -48,7 +49,6 @@ apt_deps=(
 	"telnet"
 	"sshpass"
 	"screenfetch"
-	"ripgrep"
 	"rofi"
 	"redis-server"
 	"redis-tools"
@@ -73,7 +73,6 @@ install() {
 		fi
 	done
 
-	
 	for dep in "${snap_deps[@]}"; do
 		echo "Installing snap package [$dep]..."
 		sudo snap install "$dep" --classic >>"$log_file" 2>&1
@@ -83,6 +82,39 @@ install() {
 			echo "Failed to install $dep. Check $log_file for details."
 		fi
 	done
+
+	script_dir=$(dirname "$(realpath "$0")")
+	base_dir=$(realpath "$script_dir/..")
+
+	echo "Creating symbolic links for dotfiles..."
+
+	declare -A dotfiles=(
+		["alacritty"]=".alacritty"
+		["i3"]=".i3"
+		["i3blocks"]=".i3blocks"
+		["nvim"]=".config/nvim"
+		["polybar"]=".config/polybar"
+		["rofi"]=".config/rofi"
+		["scripts"]=".scripts"
+		["tmux"]=".tmux.conf"
+	)
+
+	for src in "${!dotfiles[@]}"; do
+		dest="${dotfiles[$src]}"
+		src_path="$base_dir/$src"
+		dest_path="$HOME/$dest"
+
+		mkdir -p "$(dirname "$dest_path")"
+
+		if [ ! -L "$dest_path" ]; then
+			ln -s "$src_path" "$dest_path"
+			echo "Linked $src to $dest_path"
+		else
+			echo "Symbolic link for $dest_path already exists"
+		fi
+	done
+
+	echo "Symbolic links created successfully."
 
 	echo "Installation completed."
 	echo "=============================================" >>"$log_file"
@@ -94,3 +126,4 @@ main() {
 }
 
 main "$@"
+
